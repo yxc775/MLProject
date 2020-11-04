b082cd25193b5a4274460d9ea1605d42d679c3ac
yxc775 <yxc775@case.edu>
2020-11-04 15:57:14 -0500

    improve discriminator models

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -33,16 +33,13 @@ def define_model(in_shape=(28,28,1)):
     model.add(Conv2D(64, (4, 4), strides=(2, 2), padding='same', input_shape=in_shape))
     model.add(ReLU())
     model.add(Dropout(0.4))
-    model.add(Conv2D(64, (3, 3), strides=(2, 2), padding='same'))
-    model.add(ReLU())
-    model.add(Dropout(0.4))
-    model.add(Conv2D(64, (2, 2), strides=(1, 1), padding='same'))
+    model.add(Conv2D(64, (4, 4), strides=(2, 2), padding='same'))
     model.add(ReLU())
     model.add(Dropout(0.4))
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid'))
     # compile model
-    opt = Adam(lr=0.0002, beta_1=0.05)
+    opt = Adam(lr=0.0002, beta_1=0.5)
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
     return model
 
